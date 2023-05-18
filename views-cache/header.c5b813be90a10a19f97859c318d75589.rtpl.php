ae2f5f30e2aa7987f364c21780f6ab635cbee28d
walan-christian128 <walancristiano@yahoo.com.br>
2023-05-18 18:39:42 -0300

    criando classe page

---
+++ views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,13 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="UTF-8">
+<title>WSstore</title>
+
+  </head>
+   <body>
+    <h1>Hello</h1>
+   </body>
+
+</html>
