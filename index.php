9b59bc723524e22f669f3dd87314846a290d09da
walan-christian128 <walancristiano@yahoo.com.br>
2023-07-17 17:39:36 -0300

    crduproduto

--- index.php
+++ index.php
@@ -285,13 +285,67 @@ $app->get("/admin/products/create", function(){
 
 $app->post("/admin/products/create", function(){
    User::verifyLogin();
+
     $product = new Products();
+
     $product -> setData($_POST);
+
     $product ->save();
+
     header("Location: /admin/products");
     exit;
 
 });
+
+$app->get("/admin/products/:idproduct", function($idproduct){
+
+    User::verifyLogin();
+
+    $product = new Products();
+
+    $product->get((int)$idproduct);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("products-update", [
+        'product'=>$product->getValues()
+    ]);
+
+});
+$app->post("/admin/products/:idproduct", function($idproduct){
+
+    User::verifyLogin();
+
+    $product = new Products();
+
+    $product->get((int)$idproduct);
+
+    $product -> setData($_POST);
+
+    $product -> save();
+
+    $product ->setPhoto($_FILES['file']);
+
+    header("Location: /admin/products");
+    exit;
+  
+
+});
+$app->get("/admin/products/:idproduct/delete", function($idproduct){
+
+    User::verifyLogin();
+
+    $product = new Products();
+
+    $product->get((int)$idproduct);
+      
+    $product ->delete();
+
+     header("Location: /admin/products");
+    exit;
+    
+});
+
 $app ->run ();
 
 ?>
