80edfc4e43c63114f0b1bada9b36f4080a9b1698
walan-christian128 <walancristiano@yahoo.com.br>
2023-07-20 18:31:05 -0300

    carri-clacular-frente

--- views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -106,7 +106,7 @@
                 <div class="navbar-collapse collapse">
                     
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="/">Home</a></li>
+                        <li class="active"><a href="">Home</a></li>
                         <li><a href="#">Produtos</a></li>
                         <li><a href="/cart">Carrinho</a></li>
                     </ul>
